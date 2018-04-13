8ab5aefee1ff178d7477ffd71907337ceeba22bc
WJohnson1 <williamisamazing1@gmail.com>
2018-04-13 11:46:27 -0400

    Added more to the Game class

--- src/student/Game.java
+++ src/student/Game.java
@@ -1,17 +1,85 @@
 package student;
 
 import javafx.application.Application;
-import model.*;
+import model.Deck;
+import model.Player;
+import model.RailroadBaronsException;
+import model.RailroadBaronsObserver;
 
-public class Game {
-    public Game(String mMaker, String rBarons){
+import java.util.Collection;
 
-    }
-    public Game(String mMaker, String rBarons,String map){
+public class Game implements model.RailroadBarons{
+     private RailroadMap railroadMap;
+     public Game(String mMaker, String rBarons){
 
-    }
-    public static void main(String[] args) {
-        Application.launch(view.RailroadBaronsUI.class,args);
-    }
+     }
+     public Game(String mMaker, String rBarons,String map){
+
+     }
+
+
+     @Override
+     public void addRailroadBaronsObserver(RailroadBaronsObserver observer) {
+
+     }
+
+     @Override
+     public void removeRailroadBaronsObserver(RailroadBaronsObserver observer) {
+
+     }
+
+     @Override
+     public void startAGameWith(model.RailroadMap map) {
+
+     }
+
+     @Override
+     public void startAGameWith(model.RailroadMap map, Deck deck) {
+
+     }
+
+     @Override
+     public model.RailroadMap getRailroadMap() {
+          return null;
+     }
+
+     @Override
+     public int numberOfCardsRemaining() {
+          return 0;
+     }
+
+     @Override
+     public boolean canCurrentPlayerClaimRoute(int row, int col) {
+          return false;
+     }
+
+     @Override
+     public void claimRoute(int row, int col) throws RailroadBaronsException {
+
+     }
+
+     @Override
+     public void endTurn() {
+
+     }
+
+     @Override
+     public Player getCurrentPlayer() {
+          return null;
+     }
+
+     @Override
+     public Collection<Player> getPlayers() {
+          return null;
+     }
+
+     @Override
+     public boolean gameIsOver() {
+          return false;
+     }
+
+     public static void main(String[] args) {
+          Application.launch(view.RailroadBaronsUI.class,args);
+     }
 }
 
