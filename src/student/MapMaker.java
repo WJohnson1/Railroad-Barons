34723050b138b5757ac1a92c95fdc7d8738b8c6d
Gabriel Jusino <gej9887@rit.edu>
2018-04-14 15:17:17 -0400

    Working on classes related to the map.

--- src/student/MapMaker.java
+++ src/student/MapMaker.java
@@ -1,20 +1,94 @@
 package student;
 
+import model.Baron;
 import model.RailroadBaronsException;
 import model.RailroadMap;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MapMaker implements model.MapMaker {
+    private HashMap<Integer, Station> stations = new HashMap<>();
+    private ArrayList<Route> routes = new ArrayList<>();
+
+    /**
+     * Loads a {@linkplain RailroadMap map} using the data in the given
+     * {@linkplain InputStream input stream}.
+     *
+     * @param in The {@link InputStream} used to read the {@link RailroadMap
+     *           map} data.
+     * @return The {@link RailroadMap map} read from the given
+     * {@link InputStream}.
+     * @throws RailroadBaronsException If there are any problems reading the
+     *                                 data from the {@link InputStream}.
+     */
     @Override
     public RailroadMap readMap(InputStream in) throws RailroadBaronsException {
         BufferedReader file = new BufferedReader(new InputStreamReader(in));
-        return null;
+        try {
+            String line = file.readLine();
+            Boolean isRoutes = false;
+            while ( line != null) {
+                String[] cut = line.split(" ");
+                if (!line.equals("##ROUTES##") && !isRoutes) {
+                    int id = Integer.parseInt(cut[0]);
+                    int row = Integer.parseInt(cut[1]);
+                    int col = Integer.parseInt(cut[2]);
+                    String name = "";
+                    for (int i = 3; i < cut.length; i++) {
+                        name = name + cut[i];
+                        if (i != (cut.length - 1)) {
+                            name = name + " ";
+                        }
+                    }
+                    stations.put(id, new Station(name, row, col));
+                }
+                else if (line.equals("##ROUTES##")){
+                    isRoutes = true;
+                }
+                else{
+                    Baron owner;
+                    if (cut[2].equals("UNCLAIMED")){
+                        owner = Baron.UNCLAIMED;
+                    }
+                    else if (cut[2].equals("RED")){
+                        owner = Baron.RED;
+                    }
+                    else if (cut[2].equals("GREEN")){
+                        owner = Baron.GREEN;
+                    }
+                    else if (cut[2].equals("YELLOW")){
+                        owner = Baron.YELLOW;
+                    }
+                    else {
+                        owner = Baron.BLUE;
+                    }
+                    routes.add(new Route(Integer.parseInt(cut[0]), Integer.parseInt(cut[1]), owner, this.stations));
+                }
+                line = file.readLine();
+            }
+            return null;
+        }
+        catch(IOException e){
+            e.printStackTrace();
+            return null;
+        }
     }
 
+    /**
+     * Writes the specified {@linkplain RailroadMap map} in the Railroad
+     * Barons map file format to the given {@linkplain OutputStream output
+     * stream}. The written map should include an accurate record of any
+     * routes that have been claimed, and by which {@linkplain Baron}.
+     *
+     * @param map The {@link RailroadMap map} to write out to the
+     *            {@link OutputStream}.
+     * @param out The {@link OutputStream} to which the
+     *            {@link RailroadMap map} data should be written.
+     * @throws RailroadBaronsException If there are any problems writing the
+     *                                 data to the {@link OutputStream}.
+     */
     @Override
     public void writeMap(RailroadMap map, OutputStream out) throws RailroadBaronsException {
 
---
+++ src/student/Track.java
@@ -0,0 +1,97 @@
+package student;
+
+import model.Baron;
+import model.Orientation;
+import model.Space;
+
+public class Track implements model.Track {
+    private Orientation orientation;
+    private Baron owner;
+    private Route route;
+    private int row;
+    private int col;
+
+    public Track(Orientation orientation, Baron owner, Route route, int row, int col) {
+        this.orientation = orientation;
+        this.owner = owner;
+        this.route = route;
+        this.row = row;
+        this.col = col;
+    }
+
+    /**
+     * Returns the orientation of the track; either
+     * {@linkplain Orientation#HORIZONTAL horizontal} or
+     * {@linkplain Orientation#VERTICAL vertical}. This is based on the
+     * {@linkplain Orientation orientation} of the {@linkplain Route route}
+     * that contains the track.
+     *
+     * @return The {@link Orientation} of the {@link Track}; this is the same
+     * as the {@link Orientation} of the {@link Route} that contains the
+     * track.
+     */
+    @Override
+    public Orientation getOrientation() {
+        return orientation;
+    }
+
+    /**
+     * Returns the current {@link Baron owner} of this track, either
+     * {@linkplain Baron#UNCLAIMED unclaimed} if the track has not been
+     * claimed, or the {@linkplain Baron owner} that corresponds with the
+     * color of the player that successfully claimed the
+     * {@linkplain Route route} of which this track is a part.
+     *
+     * @return The {@link Baron} that has claimed the route of which this
+     * track is a part.
+     */
+    @Override
+    public Baron getBaron() {
+        return this.owner;
+    }
+
+    /**
+     * Returns the {@linkplain Route route} of which this
+     * {@linkplain Track track} is a part.
+     *
+     * @return The {@link Route} that contains this track.
+     */
+    @Override
+    public model.Route getRoute() {
+        return this.route;
+    }
+
+    /**
+     * Returns the row of the space's location in the map.
+     *
+     * @return The row of the space's location in the map.
+     */
+    @Override
+    public int getRow() {
+        return this.row;
+    }
+
+    /**
+     * Returns the column of the space's location in the map.
+     *
+     * @return The column of the space's location in the map.
+     */
+    @Override
+    public int getCol() {
+        return this.col;
+    }
+
+    /**
+     * Returns true if the other space is occupying the same physical location
+     * in the map as this space.
+     *
+     * @param other The other space to which this space is being compared for
+     *              collocation.
+     * @return True if the two spaces are in the same physical location (row
+     * and column) in the map; false otherwise.
+     */
+    @Override
+    public boolean collocated(Space other) {
+        return other.getCol() == this.col && other.getRow() == this.row;
+    }
+}
