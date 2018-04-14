796a662da34485c9b93ae9b88a0fe794fa9e36d0
WJohnson1 <williamisamazing1@gmail.com>
2018-04-14 10:43:18 -0400

    Added RailroadBaronPlayer Class

---
+++ src/student/RailroadBaronPlayer.java
@@ -0,0 +1,72 @@
+package student;
+
+import model.*;
+
+import java.util.Collection;
+
+public class RailroadBaronPlayer implements Player {
+    @Override
+    public void reset(Card... dealt) {
+
+    }
+
+    @Override
+    public void addPlayerObserver(PlayerObserver observer) {
+
+    }
+
+    @Override
+    public void removePlayerObserver(PlayerObserver observer) {
+
+    }
+
+    @Override
+    public Baron getBaron() {
+        return null;
+    }
+
+    @Override
+    public void startTurn(Pair dealt) {
+
+    }
+
+    @Override
+    public Pair getLastTwoCards() {
+        return null;
+    }
+
+    @Override
+    public int countCardsInHand(Card card) {
+        return 0;
+    }
+
+    @Override
+    public int getNumberOfPieces() {
+        return 0;
+    }
+
+    @Override
+    public boolean canClaimRoute(Route route) {
+        return false;
+    }
+
+    @Override
+    public void claimRoute(Route route) throws RailroadBaronsException {
+
+    }
+
+    @Override
+    public Collection<Route> getClaimedRoutes() {
+        return null;
+    }
+
+    @Override
+    public int getScore() {
+        return 0;
+    }
+
+    @Override
+    public boolean canContinuePlaying(int shortestUnclaimedRoute) {
+        return false;
+    }
+}
